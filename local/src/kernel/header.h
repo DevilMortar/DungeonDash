20558e4921a4bd7cce68758af594b62ba9a7adb9
DevilMortar <devilmortar0159@gmail.com>
2022-04-05 08:14:57 +0200

    Centralisation du header

--- local/src/kernel/header.h
+++ local/src/kernel/header.h
@@ -84,9 +84,71 @@ struct GAME
     SDL_Rect endscorerect, endbestrect, coinrect;
 };
 
+typedef enum functions functions;
+enum functions {play, skin, leave, confirm, right, left, locker};
+
+typedef struct BUTTON BUTTON;
+struct BUTTON{
+    SPRITE button_sprite;
+    enum functions function;
+    int state;
+    int menu;
+    BUTTON * next;
+};
+
+typedef struct SKIN SKIN;
+struct SKIN{
+    SPRITE skin_sprite;
+    int state;
+    int price;
+    SKIN * previous;
+    SKIN * next;
+};
+
 // SDL
 void SDL_ExitWithError(const char * message); // Quitter
+void SDL_LimitFPS(unsigned int limit);
+void SDL_initGameView(SDL_Window ** window, SDL_Renderer ** renderer);
 int startGame(SDL_Window * window, SDL_Renderer *renderer, GAME * game, PLAYER * player, LIST_OBSTACLE fireball, int Hole[5][5],COIN * coin, TEXTURE map, TEXTURE hole);
+
+//Engine
+void initGame(SDL_Renderer * renderer, GAME *game);
+int init(PLAYER *player, LIST_OBSTACLE *fireball, int *Hole, COIN *coin, SDL_Renderer *renderer, GAME * game); // Initialisation des différents objets
+PLAYER * setPlayer(int skin); // Set le player
+POSITION randomTeleport(POSITION position, int Hole[5][5]); // Renvoie une position aléatoire où il n'y a pas de trous
+void checkMovePlayer(PLAYER *player, int direction, int Hole[5][5]);
+void movePlayer(PLAYER * player, int direction, int Hole[5][5]); // Déplace le joueur dans la direction "direction", si il n'y a pas de trous
+LIST_OBSTACLE setListObstacle(LIST_OBSTACLE list); // Initialise les listes d'obstacles
+LIST_OBSTACLE newObstacle(LIST_OBSTACLE list); // Créer un obstacle
+LIST_OBSTACLE addToQueue(LIST_OBSTACLE list, OBSTACLE *new); // Ajouter un obstacle dans une file
+LIST_OBSTACLE deleteFromQueue(LIST_OBSTACLE list); // Supprime un obstacle d'une file
+bool updateFireball(OBSTACLE *obstacle); // Déplace les boules de feu actives
+bool detectColision(POSITION posplayer, POSITION posobject); // Détecte la colision 
+
+// Display
+SPRITE newSprite(SDL_Renderer *renderer, char link[255], int max, int srcsizew, int srcsizeh, int dstsize);
+TEXTURE newTexture (SDL_Renderer *renderer, char link[255], int w, int h); // Renvoie une texture initialisée
+void updateSprite(SDL_Renderer *renderer, SPRITE sprite, int direction, POSITION position, int * frame); // Affiche un sprite et modifie sa frame
+void displaySprite(SDL_Renderer *renderer, SPRITE sprite, int direction, POSITION position, int *frame); // Affiche un sprite
+TEXTURE updateTexture(SDL_Renderer *renderer, TEXTURE texture, POSITION position, int width, int height); // Affiche une texture
+void setPlayerSprite(SDL_Renderer *renderer, PLAYER *player, SKIN *skinList);
+SDL_Texture* renderWidgetText(char *message, SDL_Color color, int fontSize, SDL_Renderer *renderer, SDL_Rect * dstrect); // Renvoie le texte sous forme d'une texture
+int displayGame(SDL_Renderer *renderer, PLAYER *player, TEXTURE map, LIST_OBSTACLE fireball, COIN *coin, int Hole[5][5], TEXTURE hole, GAME *game); // Affiche le jeu
+
+// Menu
+int menu(BUTTON * button, SKIN * skinList, SDL_Renderer *renderer,  TEXTURE map, TEXTURE title, int money, int highScore);
+void displayMainMenu(BUTTON *buttonList, SKIN *skinList, SDL_Renderer *renderer, TEXTURE map, TEXTURE title); //Affiche le menu principal
+void displaySkinMenu(BUTTON *buttonList, SKIN *skinListTMP, SDL_Renderer *renderer, TEXTURE map, TEXTURE title); //Affiche le menu des skins
+SKIN * createSkin(SDL_Renderer *renderer, char link[255], SKIN * skinList, int w, int h, int x, int y, int state, int price, int srcsizew, int srcsizeh); //Créé un nouveau skin
+SKIN * addSkinInList(SKIN *skinList, SKIN *newSkin); //Ajoute un skin à la liste
+void closeSkinList(SKIN *skinList); //Relie le premier élément de la liste au dernier, pout créer une boucle
+
+//Buttons
+BUTTON * createButton(SDL_Renderer *renderer, char link[255], BUTTON * buttonList, int w, int h, int x, int y, functions function,int state, int menu, int srcsizew, int srcsizeh); //Créé un nouveau bouton
+BUTTON * addButtonInList(BUTTON * buttonList, BUTTON * newButton); //Ajoute un bouton à la liste des boutons
+int checkClickButtons(BUTTON * buttonList, SKIN *skinListTMP, int options, int x, int y); //Vérifie si le click de la souris se fait dans un bouton
+void checkOverButtons(BUTTON * buttonList, int options, int x, int y); //Vérifie si la souris survole un bouton
+void resetButtonState(BUTTON * buttonList); //Réinitialise l'état de tous les boutons à unclicked
 
 
 
