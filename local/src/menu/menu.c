5fbaf542d686675215b87e2d485dcabe1fe00b44
DevilMortar <devilmortar0159@gmail.com>
2022-04-16 18:44:04 +0200

    Data function now in menu.c

--- local/src/menu/menu.c
+++ local/src/menu/menu.c
@@ -186,3 +186,40 @@ void closeSkinList(SKIN *skinList){
     tmp->next=skinList;
     skinList->previous=tmp;
 }
+
+void saveData(SKIN *firstSkin, SAVE *dataSave, int skinNb){
+    SKIN *tmp=firstSkin;
+    FILE *saveFile;
+    saveFile=fopen("saveFile.bin", "wba");
+    if(saveFile==NULL){
+        fprintf(stderr, "Error while opening file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i=0; i<skinNb; i++){
+        dataSave->skinState[i]=tmp->state;
+        tmp=tmp->next;
+    }
+
+    fwrite(dataSave, sizeof(dataSave), 1, saveFile);
+    fclose(saveFile);
+}
+
+SAVE * recupData(int skinNb){
+    SAVE *dataSave=NULL;
+    dataSave=malloc(sizeof(SAVE));
+    FILE *saveFile;
+    saveFile=fopen("saveFile.bin", "rb");
+    if(saveFile==NULL){
+        dataSave->money=0;
+        dataSave->highscore=0;
+        dataSave->skinState[0]=1;
+        for(int i=1; i<skinNb; i++)
+            dataSave->skinState[i]=0;
+    }
+    else{
+        fread(dataSave, sizeof(dataSave), 1, saveFile);
+        fclose(saveFile);
+    }
+    return dataSave;
+}
