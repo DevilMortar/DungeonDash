ca94a15535e7fafb033e4cbe96dd83e3bd2bf8a9
Rémi-Vassoille <remi.vassoille@isen.yncrea.fr>
2022-04-19 18:29:52 +0200

    modified menu function

--- local/src/menu/menu.c
+++ local/src/menu/menu.c
@@ -1,6 +1,6 @@
 #include "../kernel/header.h"
 
-int menu(BUTTON * buttonList, SKIN * skinList, SDL_Renderer *renderer, TEXTURE map, TEXTURE title){
+int menu(BUTTON * buttonList, SKIN * skinList, SDL_Renderer *renderer, TEXTURE map, TEXTURE title, GAME *game){
     SDL_bool menu_active = SDL_TRUE;
     int options=-1;
     int skinChoice=1;
@@ -42,14 +42,14 @@ int menu(BUTTON * buttonList, SKIN * skinList, SDL_Renderer *renderer, TEXTURE m
                 break;
             case -3:
                 skinListTMP=skinListTMP->previous;
-                if(skinListTMP->price<0 && 1>skinListTMP->price);
+                if(skinListTMP->price<0 && game->best>abs(skinListTMP->price))
                     skinListTMP->state=1;
                 displaySkinMenu(buttonList, skinListTMP, renderer, map, title);
                 options=-2;
                 break;
             case -4:
                 skinListTMP=skinListTMP->next;
-                if(skinListTMP->price<0 && 1>floor(skinListTMP->price))
+                if(skinListTMP->price<0 && game->best>abs(skinListTMP->price))
                     skinListTMP->state=1;
                 displaySkinMenu(buttonList, skinListTMP, renderer, map, title);
                 options=-2;
@@ -66,7 +66,7 @@ int menu(BUTTON * buttonList, SKIN * skinList, SDL_Renderer *renderer, TEXTURE m
                 }
                 break;
             case -6:
-                if(skinListTMP->state==0 && skinListTMP->price > 0 && skinListTMP->price <= 1){
+                if(skinListTMP->state==0 && skinListTMP->price > 0 && skinListTMP->price <= game->money){
                     skinListTMP->state=-1;
                 }
                 options=-2;
