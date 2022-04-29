6a1df02fac1c300648d2e9c26278aba937f1405d
DevilMortar <devilmortar0159@gmail.com>
2022-04-29 22:47:35 +0200

    new functions in display.h

--- local/src/display/display.h
+++ local/src/display/display.h
@@ -34,7 +34,9 @@ SPRITE newSprite(SDL_Renderer *renderer, char link[255], int max, int srcsizew, 
 TEXTURE newTexture (SDL_Renderer *renderer, char link[255], int w, int h); // Renvoie une texture initialisée
 void updateSprite(SDL_Renderer *renderer, SPRITE sprite, int direction, POSITION position, int * frame); // Affiche un sprite et modifie sa frame
 void displaySprite(SDL_Renderer *renderer, SPRITE sprite, int direction, POSITION position, int *frame); // Affiche un sprite
+void updateSpriteIfNeeded(SDL_Renderer *renderer, SPRITE sprite, int direction, POSITION position, int *frame, int loop); // Affiche un sprite et modifie sa frame si besoin
 TEXTURE updateTexture(SDL_Renderer *renderer, TEXTURE texture, POSITION position, int width, int height); // Affiche une texture
 SDL_Texture* renderWidgetText(char *message, SDL_Color *color, int fontSize, SDL_Renderer *renderer, SDL_Rect * dstrect); // Renvoie le texte sous forme d'une texture
 void displayNumber(SDL_Renderer *renderer, int number, SDL_Color *color, int fontSize, SDL_Rect *dstrect); // Affiche un nombre 
+void displayTextAndNumber(SDL_Renderer *renderer, char *message, int number, SDL_Color* color, int fontSize, SDL_Rect *dstrect); // Affiche un texte et un nombre
 int numberOfDigit(int number); // Renvoie le nombre de chiffre d'un nombre
